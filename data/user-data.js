a434026b84e5fb189b044b11c5b7fe839af69f48
Shakeel Ansari <ShakeelAnsari63@Gmail.com>
2024-05-08 22:40:42 +0530

    deploy to gh

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'shakeelansari63',
+  devUsername: "shakeelansari63",
+  github: 'https://github.com/shakeelansari63',
+  facebook: 'https://www.facebook.com/shakeelansari63/',
+  linkedIn: 'https://www.linkedin.com/in/shakeelansari63/',
+  twitter: 'https://twitter.com/shakeelansari63',
+  stackOverflow: 'https://stackoverflow.com/users/8178487/shakeel-ansari',
+  leetcode: "https://leetcode.com/shakeelansari63/",
+  badges: "https://credly.com/users/shakeelansari63",
+  skills: ['Data Engineering', 'SQL', 'Python', 'Spark', 'Teradata', 'ML', 'Azure Synapse', 'Hadoop', 'Airflow', 'Docker', 'Kubernetes', 'Azure'],
+  timezone: '+5.5'
+};
